cb1681bd3f57208c994069b47aeb600cd06da992
Dmitry Grigorev <dmitryandreevichgr@gmail.com>
2020-04-20 09:29:50 +0300

    Skeleton ready

--- CodeContracts/ICodeManager.cs
+++ CodeContracts/ICodeManager.cs
@@ -1,10 +1,26 @@
-﻿namespace CSharp.CodeUtils.CodeContracts
+﻿using System.Collections.Generic;
+using CSharp.CodeUtils.CodeContracts.CodeObjects;
+
+namespace CSharp.CodeUtils.CodeContracts
 {
     /// <summary>
     /// Source code manager
     /// </summary>
-    public interface ICodeManager
+    public interface ICodeManager : ICodeRunner
     {
-        
+        /// <summary>
+        /// Code objects collection
+        /// </summary>
+        IEnumerable<ICodeObject> CodeObjects { get; set; }
+
+        /// <summary>
+        /// Source code compilation service
+        /// </summary>
+        ICompilerService CompilationService { get; set; }
+
+        /// <summary>
+        /// Initializes code manager
+        /// </summary>
+        void Initialize();
     }
 }
